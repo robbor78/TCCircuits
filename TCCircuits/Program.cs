c9ae8dfc06420c357a5a075d57eb91601a76a04e
username <email@email.com>
2016-02-26 15:20:21 +0200

    init

---
+++ TCCircuits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCCircuits
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+
+    public int howLong(String[] connects, String[] costs)
+    {
+      return -1;
+    }
+  }
+}
