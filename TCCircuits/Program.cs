dce584379c4a785a04c7d585237b5d5e3a491a1b
username <email@email.com>
2016-02-26 15:57:52 +0200

    initial code

--- TCCircuits/Program.cs
+++ TCCircuits/Program.cs
@@ -14,7 +14,65 @@ namespace TCCircuits
 
     public int howLong(String[] connects, String[] costs)
     {
-      return -1;
+      int[][] graph = BuildGraph(connects, costs);
+
+      int max = 0;
+
+      int numNodes = graph.Length;
+
+      for (int node = 0; node < numNodes; node++)
+      {
+        int[] links = graph[node];
+        int numLinks = links.Length;
+        for (int link = 0; link < numLinks; link++)
+        {
+          Stack<int> stack = new Stack<int>();
+          stack.Push(link);
+
+          int pathLength = 0;
+          while (stack.Count() > 0)
+          {
+            int current = stack.Pop();
+
+            pathLength += graph[node][current];
+
+            int[] neighbors = graph[link];
+            Array.ForEach(neighbors, n => stack.Push(n));
+
+          }
+
+          if (pathLength > max)
+          {
+            max = pathLength;
+          }
+        }
+
+      }
+
+      return max;
+    }
+
+    private int[][] BuildGraph(string[] connects, string[] costs)
+    {
+      int numNodes = connects.Length;
+      int[][] graph = new int[numNodes][];
+
+      for (int i = 0; i < numNodes; i++)
+      {
+        graph[i] = Enumerable.Repeat(-1, numNodes).ToArray();
+
+        int[] connect = connects[i].Split().Select(x => int.Parse(x)).ToArray();
+        int[] linkCosts = costs[i].Split().Select(x => int.Parse(x)).ToArray();
+
+        int numLinks = connect.Length;
+        for (int j = 0; j < numLinks; j++)
+        {
+          int link = connect[j];
+          int cost = linkCosts[j];
+          graph[i][link] = cost;
+        }
+      }
+      return graph;
     }
   }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TCCircuits;
 
 namespace UnitTestProject1
 {
@@ -9,6 +10,34 @@ namespace UnitTestProject1
     [TestMethod]
     public void TestMethod1()
     {
+      string[][] allConnects = new string[][] {
+        new string[] {
+        "1 2",
+ "2",
+ ""}
+      };
+      string[][] allCosts = new string[][] {
+        new string[] {
+        "5 3",
+ "7",
+ ""}
+      };
+
+      int[] allLengths = new int[] { 12 };
+
+      Program p = new Program();
+      int length = allConnects.Length;
+      for (int i = 0; i < length; i++)
+      {
+        string[] connects = allConnects[i];
+        string[] costs = allCosts[i];
+
+        int actual = p.howLong(connects, costs);
+
+        int expected = allLengths[i];
+
+        Assert.AreEqual(expected, actual);
+      }
     }
   }
 }
